85870cc990f632488d0266a77aec94864390fa92
Leonardo Dias <contato.leonardod@yahoo.com>
2020-03-03 20:11:55 -0300

    create web client project  and some lint and deps configs

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Home = () => {
+  return (
+    <Container>
+      <h2>hello my friend</h2>
+    </Container>
+  );
+};
+
+export default Home;
